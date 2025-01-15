9251becf5d5346f6beb2fd67cbb930fba7d2bfa6
Mishka <38524387+Namarimizu@users.noreply.github.com>
2025-01-15 16:14:46 +0800

    Add files via upload

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -328,11 +328,11 @@ const lv_img_dsc_t octoling = {
 
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_YUKI uint8_t YUKI_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
-        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
-        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
-#else
         0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
         0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
+#else
+        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
+        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
 #endif
 
 
